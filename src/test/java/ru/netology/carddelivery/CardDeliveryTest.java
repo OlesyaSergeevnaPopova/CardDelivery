2c9062488a464fe65c97e93aca9d6d3a7706f2c4
OlesyaSergeevnaPopova <lesenok170389@rambler.ru>
2022-05-30 22:55:03 +0300

    Updated HW

--- src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
@@ -1,10 +1,14 @@
 package ru.netology.carddelivery;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -13,26 +17,29 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
+
 public class CardDeliveryTest {
+    public String generateDate(int days){
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
 
     @Test
     void shouldRegister() {
-        Configuration.holdBrowserOpen=true;
+        Configuration.holdBrowserOpen = true;
 
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yy");
-        String date_now = formatter.format(date);
-        String str = formatter.format(date);
+        String date_event = generateDate(5);
 
         open("http://localhost:9999");
         $("[placeholder=\"Город\"]").setValue("Ростов-на-Дону");
-        $("[placeholder=\"Дата встречи\"]").setValue(str);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(date_event);
         $("[name=\"name\"]").setValue("Иванов Иван");
         $("[name=\"phone\"]").setValue("+79001112233");
         $("[data-test-id=\"agreement\"]").click();
         $(byText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована")).should(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date_event), Duration.ofSeconds(15));
         //Встреча успешно забронирована на
 
     }
